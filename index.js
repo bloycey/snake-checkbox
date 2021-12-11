0cdf91ec818d193c92a02ac38d9a93a2c9efedf6
Andrew Bloyce <bloyce1990@gmail.com>
2021-12-11 22:21:16 +1000

    lil trick for mobile vh units

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ const GAME_DATA = {
 	bestScore: 0
 }
 
+// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 window.addEventListener('DOMContentLoaded', (event) => {
 	loadCheckboxes()
 		.then(() => {
